eb4c43cabfb24e3e2cffff4dbe8cec6223f94a3c
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2022-03-04 11:46:48 +0100

    Added files

---
+++ CodeChallenge8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeChallenge8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountTripLet(new[] { 4, 0, 1, 3, 2 }, new[] { 4, 1, 0, 2, 3 }));
+        }
+
+
+        static int CountTripLet(int[] nums1, int[] nums2)
+        {
+            var result = GetCombinations(Enumerable.Range(0, nums1.Length), 3).Where(r => r.Distinct().Count() == 3);
+
+            var validListCount = result
+                .Select(triplet =>
+                    triplet.ToList())
+                .Count(t => Array.IndexOf<int>(nums1, t[0]) < Array.IndexOf<int>(nums1, t[1])
+                            && Array.IndexOf<int>(nums1, t[1]) < Array.IndexOf<int>(nums1, t[2])
+                            && Array.IndexOf<int>(nums2, t[0]) < Array.IndexOf<int>(nums2, t[1])
+                            && Array.IndexOf<int>(nums2, t[1]) < Array.IndexOf<int>(nums2, t[2]));
+            
+            return validListCount;
+        }
+
+
+        static IEnumerable<IEnumerable<T>> GetCombinations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            return GetCombinations(list, length - 1)
+                .SelectMany(t => list, (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+    }
+}
---
+++ Partition/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Partition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GetPivots(new[] { 22, 4, -33, -20, -15, 15, -16, 7, 19, -10, 0, -13, -14 });
+
+            // GetMaximunPivotCount(new[] { 22, 4, -25, -20, -15, 15, -16, 7, 19, -10, 0, -13, -14 }, -33);
+            // GetMaximunPivotCount(new[] { 22, 4, -25, -20, -15, 15, -16, 7, 19, -10, 0, -13, -14 }, -33);
+            GetMaximunPivotCount(new[] { 2,-1,2 }, 3);
+        }
+
+        static int GetMaximunPivotCount(int[] nums, int k)
+        {
+            int max = GetPivots(nums.ToList());
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var numList = nums.ToList();
+                numList.Insert(i, k);
+                numList.RemoveAt(i + 1);
+                 
+                max = Math.Max(max, GetPivots(numList));
+                
+            }
+            
+            return max;
+        }
+
+
+        static int GetPivots(List<int> nums)
+        {
+            var pivots = new List<int>();
+            for (int i = 1; i < nums.Count; i++)
+            {
+                var preAdd = 0;
+                for (int j = 0; j < i; j++) preAdd += nums[j];
+                
+                var postAdd = 0;
+
+                for (int j = i; j < nums.Count; j++) postAdd += nums[j];
+                
+                if(preAdd == postAdd) pivots.Add(nums[i]);
+            }
+
+            return pivots.Count;
+        }
+    }
+}
